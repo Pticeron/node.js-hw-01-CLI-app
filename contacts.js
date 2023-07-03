692dae79a7be83370d5cabdd4054a03eab36f5fd
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-07-03 20:24:59 +0300

    add contacts

--- contacts.js
+++ contacts.js
@@ -1,31 +1,65 @@
-const { log } = require("console");
 const fs = require("fs/promises");
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "contacts.json");
-console.log(contactsPath);
-
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
-}
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log("Error: ${error.message}".red);
+  }
+};
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === contactId);
+    return result || null;
+  } catch (error) {
+    console.log("Error: ${error.message}".red);
+  }
+};
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const indexContact = contacts.findIndex((item) => item.id === contactId);
+    if (indexContact === -1) {
+      return null;
+    }
+    const [deleteContact] = contacts.splice(indexContact, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return deleteContact;
+  } catch (error) {
+    console.log("Error: ${error.message}".red);
+  }
+};
 
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
-}
+const addContact = async (name, email, phone) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    const updateContacts = [newContact, ...contacts];
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.log("Error: ${error.message}".red);
+  }
+};
+
+listContacts();
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-} 
+};
--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-const contacts = require("./contacts");
+const { listContacts, getContactById, removeContact, addContact } = require("./contacts");
 
+const { Command } = require("commander");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
-      const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
+      const contacts = await contacts.listContacts();
+      console.table(contacts);
       break;
 
-    // case "get":
-    //   // ... id
-    //   break;
+    case "get":
+      const contact = await contacts.getContactById(id);
+      console.table(contacts);
+      break;
 
-    // case "add":
-    //   // ... name email phone
-    //   break;
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.table(newContact);
+      break;
 
-    // case "remove":
-    //   // ... id
-    //   break;
+    case "remove":
+      const remContact = await contacts.removeContact(id);
+      break;
 
-    // default:
-    //   console.warn("\x1B[31m Unknown action type!");
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
-invokeAction({action: "list"});
+
+invokeAction(argv);
